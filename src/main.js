97003b7ddea0e215ab1e53d8d4f6e72f344cb408
tflin <tflins@163.com>
2019-04-11 16:46:59 +0800

    初始化项目结构

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
 
 Vue.config.productionTip = false
+
+Vue.use(MuseUI)
 
 new Vue({
   router,
